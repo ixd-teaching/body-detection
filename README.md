9974501d8055579ba35adca383e013e5773bd8f1
Jens Pedersen <jenspedersen@gmail.com>
2020-10-12 18:09:43 +0200

    bug fixes and rudolph -> drunk rudolph

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-### web-framework to play with dynamic body pose detection
+### Web framework to play with dynamic body pose detection
+
+#### Description
+
+The framework uses tenserflow.js/posenet to detect one or more bodies from the browser's camera and emmits positional data for each body's body parts. The framework can use either of two model architectures from posenet: 'MobileNetV1' (default) or 'ResNet50'. ResNet50 is more precise, but also slower and consumes more resources. The framework can be configured to detect either a single body or multiple bodies in the camera stream. When one or more bodies are detected an array of body data is emitted to all listeners.
+
+### Usage
+
+To setup body detection instatiate a new object of the class 'Bodystream':
+_Example_
+    const bodies = new BodyStream (posenet, detectionType.singleBody, document.getElementById('video'), modelType.samplingRate)
 
 Body-detection in /lib/bodydetection.js
 
 Examples sketches are in /Sketches
-
--- sketches/wrist-distance/wrist-distance.js
+++ sketches/wrist-distance/wrist-distance.js
@@ -1,10 +1,53 @@
 // @ts-nocheck
-const samplingRate = 250
-const bodies = new BodyStream (posenet, detectionType.singleBody, document.getElementById('video'), samplingRate)
+
+// sets up a bodystream with configuration object
+const bodies = new BodyStream ({
+      posenet: posenet,
+      architecture: modelArchitecture.MobileNetV1, 
+      detectionType: detectionType.singleBody, 
+      videoElement: document.getElementById('video'), 
+      samplingRate: 250})
+    
+let body
 
 bodies.addEventListener('bodiesDetected', (e) => {
-    const body = e.detail.bodies.getBodyAt(0)
-    console.log(body.getDistanceBetweenBodyParts(bodyParts.leftWrist, bodyParts.rightWrist))
+    body = e.detail.bodies.getBodyAt(0)
+    const distance = Math.round(body.getDistanceBetweenBodyParts(bodyParts.leftWrist, bodyParts.rightWrist))
+    document.getElementById('output').innerText = `Distance between wrists: ${distance}`
+    body.getDistanceBetweenBodyParts(bodyParts.leftWrist, bodyParts.rightWrist)
 })
+
+// get elements
+let video = document.getElementById("video");
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+// draw the video, nose and eyes into the canvas
+function drawCameraIntoCanvas() {
+
+    // draw the video element into the canvas
+    ctx.drawImage(video, 0, 0, video.width, video.height);
+    
+    if (body) {
+        // draw circle for left and right wrist
+        const leftWrist = body.getBodyPart(bodyParts.leftWrist)
+        const rightWrist = body.getBodyPart(bodyParts.rightWrist)
+
+        // draw left wrist
+        ctx.beginPath();
+        ctx.arc(leftWrist.position.x, leftWrist.position.y, 10, 0, 2 * Math.PI);
+        ctx.fillStyle = 'white'
+        ctx.fill()
+
+        // draw right wrist
+        ctx.beginPath();
+        ctx.arc(rightWrist.position.x, rightWrist.position.y, 10, 0, 2 * Math.PI);
+        ctx.fillStyle = 'white'
+        ctx.fill()
+    }
+    requestAnimationFrame(drawCameraIntoCanvas)
+}
+
+requestAnimationFrame(drawCameraIntoCanvas)
 
 bodies.start()
