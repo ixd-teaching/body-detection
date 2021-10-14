ee3a2847298b02493cd8f46b1d265d959fdd5d64
helmersson <50464822+helmersson@users.noreply.github.com>
2021-10-14 10:53:27 +0200

    ^^

---
+++ public/sketches/wrist-distance2/README.md
@@ -0,0 +1,36 @@
+## Distance between bodyparts (wrists)
+
+### Description
+This sketch demonstrates how to get the distance between two body parts in this case left and right wrists.
+
+### Usage 
+As ususal we get a camera feed, configure the framework and get the body data from the event handler handed 'detectBodies' and store the latest body in 'latestBody'.
+
+~~~javascript
+const video = await createCameraFeed(canvas.width, canvas.height, facingMode.environment)
+  let latestBody
+
+  // create a video element connected to the camera
+  const video = await createCameraFeed(canvas.width, canvas.height, facingMode.environment)
+
+  const config = {
+      video: video,
+      multiPose: false,
+      sampleRate: 100,
+      flipHorizontal: true // true if webcam
+
+  }
+  // start detecting bodies camera-feed a set latestBody to first (and only) body
+  detectBodies(config, (e) => latestBody = e.detail.bodies.listOfBodies[0])
+~~~
+
+We manually draw each frame onto a canvas element with data overlaid on top of it and output the distance between wrists as well continuously:
+
+~~~javascript
+  // draw video with wrists overlaid onto canvas continuously
+  continuosly(() => {
+      drawImageWithOverlay(canvas, video, () => drawWrists(canvas, latestBody))
+      outputDistance(status, latestBody)
+  })
+~~~
+
---
+++ public/sketches/wrist-distance2/script.mjs
@@ -0,0 +1,65 @@
+import { detectBodies, bodyPartsList } from "../../lib/bodydetection.mjs"
+import { drawImageWithOverlay, drawSolidCircle } from "../../lib/drawing.mjs"
+import { continuosly } from "../../lib/system.mjs"
+import { createCameraFeed, facingMode } from "../../lib/camera.mjs"
+
+
+function outputDistance(status, body) {
+    if (body) {
+        const distance = body.getDistanceBetweenBodyParts3D(bodyPartsList.leftWrist, bodyPartsList.rightWrist)
+        status.innerText = `Distance between wrists: ${distance.toFixed(2)} m`
+    }
+}
+
+function changeColor(outputDistance) {
+    if (distance <= 0.5){
+        document.body.style.backgroundColor = "red";
+    } else {
+        document.body.style.backgroundColor = "white";
+    }
+    console.log(distance.toFixed(2));
+}
+
+function drawWrists(canvas, body) {
+    if (body) {
+        // draw circle for left and right wrist
+        const leftWrist = body.getBodyPart2D(bodyPartsList.leftWrist)
+        const rightWrist = body.getBodyPart2D(bodyPartsList.rightWrist)
+
+        // draw left wrist
+        drawSolidCircle(canvas, leftWrist.position.x, leftWrist.position.y, 10, 'white')
+
+        // draw right wrist
+        drawSolidCircle(canvas, rightWrist.position.x, rightWrist.position.y, 10, 'white')
+    }
+}
+
+async function run(canvas, status) {
+    let latestBody
+
+    // create a video element connected to the camera
+    const video = await createCameraFeed(canvas.width, canvas.height, facingMode.environment)
+
+    const config = {
+        video: video,
+        multiPose: false,
+        sampleRate: 100,
+        flipHorizontal: true // true if webcam
+
+    }
+    // start detecting bodies camera-feed a set latestBody to first (and only) body
+    detectBodies(config, function (e) 
+    { 
+        latestBody = e.detail.bodies.listOfBodies[0] 
+    })
+
+    // draw video with wrists overlaid onto canvas continuously
+    continuosly(() => {
+        drawImageWithOverlay(canvas, video, () => drawWrists(canvas, latestBody))
+        outputDistance(status, latestBody)
+    })
+
+    changeColor();
+}
+
+export { run }
